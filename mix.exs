b82114a158e3b6a1d36c1413fa372ac06518bc37
Art Peel <art@compound.finance>
2018-08-06 17:16:17 -0700

    bump version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule PlugProtobufs.Mixfile do
 
   def project do
     [app: :plug_protobufs,
-     version: "0.1.0",
+     version: "0.1.1",
      elixir: "~> 1.3",
      description: description(),
      package: package(), 
